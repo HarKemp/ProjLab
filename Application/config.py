6d39f4f2cafee267bc18f87cddb7c707e90ed758
Haralds <haralds.kempelis@edu.rtu.lv>
2024-10-08 19:51:28 +0300

    updated config

--- Application/config.py
+++ Application/config.py
@@ -1,4 +1,13 @@
+import os
+
 class Config(object):
-    SECRET_KEY = 'af28j093jf9wjfp9@J@*sajfaioj' # pagaidām šo nevajag
+    SECRET_KEY = 'af28j093jf9wjfp9@J@*sajfaioj'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+
+    ### Creates the instance db file in Application/instance directory
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    DATABASE_DIR = os.path.join(BASE_DIR, 'instance')
+    if not os.path.exists(DATABASE_DIR):
+        os.makedirs(DATABASE_DIR)
+        
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(DATABASE_DIR, 'test.db')}"
