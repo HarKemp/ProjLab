7d4349c7056746be0fc2384b509523ce66f47d48
Krisjanis Bergmanis <Krisjanis.Bergmanis@edu.rtu.lv>
2024-10-28 23:51:57 +0200

    ProjLab: Add units to service

--- Application/app/db/emissions.py
+++ Application/app/db/emissions.py
@@ -5,6 +5,7 @@ class Emissions(db.Model):
     __tablename__ = 'emissions'
     id = db.Column(db.Integer, primary_key=True)
     service = db.Column(db.String(80), nullable=False)
-    price = db.Column(db.Float, nullable=False)
+    service_price = db.Column(db.Float, nullable=False)
+    units_per_service = db.Column(db.Float)
     value = db.Column(db.Float, nullable=False)
     
