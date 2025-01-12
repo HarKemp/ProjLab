764fb9823eab01386f06fd2080c176bca5b663a5
Haralds <haralds.kempelis@edu.rtu.lv>
2025-01-12 23:53:32 +0200

    emission values by category

--- Application/app/database/emmision_values.py
+++ Application/app/database/emmision_values.py
@@ -1,35 +1,32 @@
 # This file holds a table that gets populated when app is launched
-# It holds the service name and emission value per 1 unit
+# It holds the service category and emission value per 1 unit
 
 import csv
 from app.__init__ import db
 
 class EmissionValue(db.Model):
-    __tablename__ = 'emission_values'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False, unique=True)
+    __tablename__ = 'emission_categories'
+    name = db.Column(db.String(100), primary_key=True)
     value = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return f"<EmissionValue name={self.name}, value={self.value}>"
+        return f"<EmissionValue name={self.name}, coefficient={self.coefficient}>"
 
 # inits table with starting values in for CO2 emissions
 def create_and_populate_table(app, csv_file_path):
     with app.app_context():
-        # Create table if not exist
-        # db.create_all()
 
         # Read the CSV and insert values
         with open(csv_file_path, encoding='utf-8-sig') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 # Strip any unwanted spaces from the header keys
-                name = row['name'].strip().lower()  # Ensure uniform casing and spacing
+                name = row['name'].strip().lower()
                 value = float(row['value'].strip())
                 # Check for duplicates before inserting
                 if not EmissionValue.query.filter_by(name=name).first():
-                    emission_values = EmissionValue(name=name, value=value)
-                    db.session.add(emission_values)
+                    category = EmissionValue(name=name, value=value)
+                    db.session.add(category)
 
         db.session.commit()
         print("Emission Value Table populated successfully!")
@@ -38,8 +35,4 @@ def create_and_populate_table(app, csv_file_path):
 def get_emission_value(name):
     # Search for names that contain the search term, case-insensitive
     result = EmissionValue.query.filter(EmissionValue.name.ilike(f'%{name.lower()}%')).first()
-
-    if result:
-        return result.value
-    else:
-        return 0
+    return result.value if result else 0
